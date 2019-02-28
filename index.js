afe3220f48407e6965ea90f0f628fe7f70731035
Kevin K <doilerjar@outlook.com>
2019-02-28 06:49:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Kevin Kang";
-const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
 const shortGreeting = "Welcome, " + currentUser.substr(0) + "!";
