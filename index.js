88c4f2c944ae4b8febae39dfef705ba56af1956f
Kevin K <doilerjar@outlook.com>
2019-02-28 06:47:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = "Kevin Kang";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const shortGreeting = "Welcome, " + currentUser.substr(0) + "!";
